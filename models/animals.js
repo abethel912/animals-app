cdd34d173dbaae257a381e69bf880b07bc929101
abethel912 <abethel180@gmail.com>
2022-11-30 16:26:16 -0500

    Animal data loaded in model

---
+++ models/animals.js
@@ -0,0 +1,64 @@
+const animals = [
+  {
+    species: 'West African Black Rhinoceros',
+    img: 'https://onekindplanet.org/wp-content/uploads/2016/11/black-rhino-yoky-e1503421255966.jpg',
+    location: [
+      'Angola',
+      'Kenya',
+      'Mozambique',
+      'Namibia',
+      'South Africa',
+      'United Republic of Tanzania',
+      'Zimbabwe',
+      'Ethiopia',
+      'Cameroon',
+      'Chad',
+      'Rwanda',
+      'Botswana',
+      'Malawi',
+      'Swaziland',
+      'Zambia'
+    ],
+    lifeExpectancy: 40,
+    isExtinct: true
+  },
+
+  {
+    species: 'Baiji White Dolphin',
+    img: 'https://onekindplanet.org/wp-content/uploads/2016/10/baji_white_dolphin.jpg',
+    location: ['Yangtze River (China)'],
+    lifeExpectancy: 24,
+    isExtinct: false
+  },
+
+  {
+    species: 'Northern Giraffe',
+    img: 'https://giraffeconservation.org/wp-content/uploads/2019/03/northern-giraffe_gcf_stock-77.jpg',
+    location: ['West Africa', 'Central Africa', 'East Africa'],
+    lifeExpectancy: 25,
+    isExtinct: false
+  },
+
+  {
+    species: 'Bald Eagle',
+    img: 'https://d18lev1ok5leia.cloudfront.net/chesapeakebay/images/09.22_bald-eagle_Issue.jpg',
+    location: ['North America'],
+    lifeExpectancy: 30,
+    isExtinct: false
+  },
+
+  {
+    species: 'Jaguar',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Standing_jaguar.jpg',
+    location: [
+      'Southwestern United States',
+      'Mexico',
+      'Central America',
+      'Amazon Rainforest'
+    ],
+    lifeExpectancy: 16,
+    isExtinct: false
+  }
+]
+
+module.exports = animals
